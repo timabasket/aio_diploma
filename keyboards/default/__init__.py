12f64a194eb47fae96c81be85c59ed50c301e471
Timofey <79515688+timabasket@users.noreply.github.com>
2022-07-29 22:56:26 +0300

    Update __init__.py

--- keyboards/default/__init__.py
+++ keyboards/default/__init__.py
@@ -1,1 +1,6 @@
 from .screens import screen1
+from .screens import screen2
+from .screens import screen_main_FAQ
+from .screens import screen_categories
+from .screens import screen_return_button
+from .screens import screen_subcategories_return
