edf4039d95562d3e03964c08fc362979f691bd45
Timofey <79515688+timabasket@users.noreply.github.com>
2022-07-29 23:00:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+TASK_SPREADSHEET = os.getenv('TASK_SPREADSHEET')
 # можно засунуть нужные ссылки для бота
 
 print(BOT_TOKEN)
+print(TASK_SPREADSHEET)
