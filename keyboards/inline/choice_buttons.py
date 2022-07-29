0f0b87c9a8196571efc38087ea57273a65b86a85
Timofey <79515688+timabasket@users.noreply.github.com>
2022-07-29 22:57:19 +0300

    Update choice_buttons.py

--- keyboards/inline/choice_buttons.py
+++ keyboards/inline/choice_buttons.py
@@ -1,25 +1,26 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from keyboards.inline.callback_datas import start_menu_callback
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import TASK_SPREADSHEET
 
 
-start_menu_keyboard = InlineKeyboardMarkup(row_width=2)
 
-tell_me_about = InlineKeyboardButton(text='Расскажи, чем полезен', callback_data=start_menu_callback.new(
-                item_name="tell_me_about"
-            ))
-start_menu_keyboard.insert(tell_me_about)
+task_manager = InlineKeyboardMarkup(row_width=2)
 
-main_FAQ = InlineKeyboardButton(text="К работе!", callback_data=start_menu_callback.new(
-                item_name="main_FAQ"
-))
-start_menu_keyboard.insert(main_FAQ)
+go_to_spreadsheet = InlineKeyboardButton(text="Таблица", url=TASK_SPREADSHEET, callback_data="spreadsheet")
+task_manager.insert(go_to_spreadsheet)
 
-# пока не использую
-main_menu_FAQ_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text='К работе!')
-        ]
-    ]
-)
+task_is_done = InlineKeyboardButton(text="Заполнил таблицу", callback_data="Done")
+task_manager.insert(task_is_done)
 
+go_to_main_menu =  InlineKeyboardButton(text="Отмена", callback_data="go_back")
+task_manager.add(go_to_main_menu)
+
+timer_keyboard = InlineKeyboardMarkup(row_width=2)
+
+start_timer = InlineKeyboardButton(text="Запустить таймер", callback_data="start_timer")
+timer_keyboard.insert(start_timer)
+
+stop_timer = InlineKeyboardButton(text="Посмотреть своё время", callback_data="stop_timer")
+timer_keyboard.insert(stop_timer)
+
+go_to_main_menu =  InlineKeyboardButton(text="Отмена", callback_data="go_back")
+timer_keyboard.add(go_to_main_menu)
