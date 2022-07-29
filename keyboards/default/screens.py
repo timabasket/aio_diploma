bf6d9bf75fcb72d9f05e865248b28b9472ed0ad6
Timofey <79515688+timabasket@users.noreply.github.com>
2022-07-29 22:55:58 +0300

    Update screens.py

--- keyboards/default/screens.py
+++ keyboards/default/screens.py
@@ -1,4 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+import random
 
 screen1 = ReplyKeyboardMarkup(
     keyboard=[
@@ -18,6 +19,27 @@ screen2 = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
+screen_main_FAQ_test = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton('📅 Создать список задач'),
+            KeyboardButton('📋 Создать КП')
+
+        ],
+        [
+            KeyboardButton('📦 Узнать информацию по товарным позициям')
+        ],
+        [
+            KeyboardButton('🕐 Подсчитать количество рабочего времени')
+        ],
+        [
+            KeyboardButton('❓ Справка')
+        ]
+    ]
+)
+
+# кнопка возврата
+return_button = KeyboardButton('В меню')
 
 
 # тут кнопки для основного меню
@@ -33,7 +55,20 @@ screen_main_FAQ.row(work_list_button, make_propose_button).add(know_item_info_bu
 # тут кнопки для блока информации по категориям
 cat1_button =  KeyboardButton ('Товары Eurotec')
 cat2_button =  KeyboardButton ('Топ-10 товаров')
+categories_return = KeyboardButton('Информация по другим категориям')
 screen_categories = ReplyKeyboardMarkup(resize_keyboard=True)
-screen_main_FAQ.row(cat1_button, cat2_button)
+screen_categories.row(cat1_button, cat2_button).add(return_button)
 
+# подкатегории раздела "Информация по товарам"
+screen_subcategories_return = ReplyKeyboardMarkup(resize_keyboard=True)
+screen_subcategories_return.row(return_button, categories_return)
+
+#постоянная кнопка
+screen_return_button = ReplyKeyboardMarkup(resize_keyboard=True)
+screen_return_button.add(return_button)
+
+#возврат в меню таймера
+timer_return_button = ReplyKeyboardMarkup(resize_keyboard=True) # клавиатура
+timer_menu_return_button = KeyboardButton('Запустить новый таймер') #кнопка
+timer_return_button.row(timer_menu_return_button, return_button)
 
